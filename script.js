e8b7d2e781b316aae4e95a56e6ce549787c44653
kchen1999 <kchen174175@gmail.com>
2022-07-03 15:55:11 +1000

    added subtract, add, multiply and divide functions

---
+++ script.js
@@ -0,0 +1,18 @@
+function add(x, y) {
+    return x + y; 
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply (x, y) {
+    return x * y;
+}
+
+function divide (x, y) {
+    const ans = x / y; 
+    return Math.round(ans * 1000) / 1000; 
+}
+
+
