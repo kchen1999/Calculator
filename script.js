dc42b219e4e03501f33bf8eab1df842ca7f31e1b
kchen1999 <kchen174175@gmail.com>
2022-07-03 15:59:03 +1000

    added operate function that takes an operator and two numbers

--- script.js
+++ script.js
@@ -6,13 +6,26 @@ function subtract(x, y) {
     return x - y;
 }
 
-function multiply (x, y) {
+function multiply(x, y) {
     return x * y;
 }
 
-function divide (x, y) {
+function divide(x, y) {
     const ans = x / y; 
     return Math.round(ans * 1000) / 1000; 
 }
 
-
+function operate(x, y, operator) {
+    if(operator === '+') {
+        return multiply(x, y); 
+    }
+    else if(operator === "-") {
+        return subtract(x, y); 
+    }
+    else if(operator === "*") {
+        return multiply(x, y); 
+    }
+    else {
+        return divide(x, y); 
+    }
+}
