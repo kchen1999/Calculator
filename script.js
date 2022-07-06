2674707d8cf018f28c64ffec314af330339d4c00
kchen1999 <kchen174175@gmail.com>
2022-07-07 02:57:42 +1000

    fixed divided by 0 bug

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(x, y) {
-    return x + y; 
+    return x + y;
 }
 
 function subtract(x, y) {
@@ -11,136 +11,145 @@ function multiply(x, y) {
 }
 
 function divide(x, y) {
-    const ans = x / y; 
-    return Math.round(ans * 1000) / 1000; 
+    const ans = x / y;
+    return Math.round(ans * 1000) / 1000;
 }
 
 function operate(x, y, operator) {
-    if(operator === '+') {
-        return add(x, y); 
+    if (operator === '+') {
+        return add(x, y);
     }
-    else if(operator === "-") {
-        return subtract(x, y); 
+    else if (operator === "-") {
+        return subtract(x, y);
     }
-    else if(operator === "x") {
-        return multiply(x, y); 
+    else if (operator === "x") {
+        return multiply(x, y);
     }
     else {
-        return divide(x, y); 
+        return divide(x, y);
     }
 }
 
 function isOperator(str) {
-    if(str === "x" || str === "÷" || str === "+" || str === "-") {
+    if (str === "x" || str === "÷" || str === "+" || str === "-") {
+        return true;
+    }
+    return false;
+}
+
+function isOperatorKey(str) {
+    if(str === "NumpadMultiply" || str === "Slash" || str === "NumpadAdd" || str === "Minus") {
         return true; 
     }
     return false; 
 }
 
 function disableDecimal(num, decimalButton) {
-    if(num.includes(".")) {
-        decimalButton.disabled = true; 
-    }   
+    if (num.includes(".")) {
+        decimalButton.disabled = true;
+    }
     else {
-        decimalButton.disabled = false; 
+        decimalButton.disabled = false;
     }
 }
 
 function populateDisplay() {
-    const equation = document.querySelector('.equation-display'); 
-    const answer = document.querySelector('.answer-display'); 
-    const decimalButton = document.getElementById("decimal"); 
-   
+    const equation = document.querySelector('.equation-display');
+    const answer = document.querySelector('.answer-display');
+    const decimalButton = document.getElementById("decimal");
+
     let firstNum = "";
     let operator = "";
-    let secondNum = ""; 
+    let secondNum = "";
     let lastValue = "firstNum";
-    let displayValue = ""; 
+    let displayValue = "";
 
-    const btns = document.querySelectorAll("button"); 
+    const btns = document.querySelectorAll("button");
     btns.forEach(btn => {
         btn.addEventListener("click", () => {
-            if(btn.textContent === "DEL") {
-                if(displayValue.length === 1) {
-                    if(lastValue === "firstNum") {
+            if (btn.textContent === "DEL") {
+                if (displayValue.length === 1) {
+                    if (lastValue === "firstNum") {
                         firstNum = "";
-                        displayValue = ""; 
+                        displayValue = "";
                     }
-                    else if(lastValue === "operator") {
-                        displayValue = displayValue.substring(0, displayValue.length - 1); 
+                    else if (lastValue === "operator") {
+                        displayValue = displayValue.substring(0, displayValue.length - 1);
                     }
                     else {
                         secondNum = "";
-                        displayValue = ""; 
+                        displayValue = "";
                     }
                 }
                 else {
-                    displayValue = displayValue.substring(0, displayValue.length - 1); 
-                    if(lastValue === "firstNum") {
+                    displayValue = displayValue.substring(0, displayValue.length - 1);
+                    if (lastValue === "firstNum") {
                         firstNum = displayValue;
                     }
-                    else if(lastValue === "secondNum") {
-                        secondNum = displayValue;; 
+                    else if (lastValue === "secondNum") {
+                        secondNum = displayValue;;
                     }
                 }
                 answer.textContent = displayValue;
             }
-            else if(btn.textContent === "AC") {
+            else if (btn.textContent === "AC") {
                 displayValue = "0";
                 firstNum = "";
                 operator = "";
                 secondNum = "";
-                decimalButton.disabled = false; 
-                equation.textContent = ""; 
+                decimalButton.disabled = false;
+                equation.textContent = "";
                 answer.textContent = displayValue;
             }
-            else if(lastValue === "secondNum" && btn.textContent === "=") {
-                displayValue = operate(parseFloat(firstNum), parseFloat(secondNum), operator) + ""; 
-                equation.textContent = firstNum + " " + operator + " " + secondNum + " ="; 
+            else if (lastValue === "secondNum" && btn.textContent === "=") {
+                displayValue = operate(parseFloat(firstNum), parseFloat(secondNum), operator) + "";
+                equation.textContent = firstNum + " " + operator + " " + secondNum + " =";
                 answer.textContent = displayValue;
                 disableDecimal(displayValue, decimalButton);
-                firstNum = displayValue; 
-                lastValue = "firstNum"; 
+                firstNum = displayValue;
+                lastValue = "firstNum";
                 operator = "";
                 secondNum = "";
             }
-            else if(lastValue === "secondNum" && isOperator(btn.textContent)) {
-                displayValue = operate(parseFloat(firstNum), parseFloat(secondNum), operator) + ""; 
-                firstNum = displayValue; 
-                lastValue = "operator"; 
+            else if (lastValue === "secondNum" && isOperator(btn.textContent)) {
+                displayValue = operate(parseFloat(firstNum), parseFloat(secondNum), operator) + "";
+                firstNum = displayValue;
+                lastValue = "operator";
                 operator = btn.textContent;
                 secondNum = "";
                 disableDecimal(displayValue, decimalButton);
-                equation.textContent = displayValue + " " + operator; 
-                answer.textContent = displayValue;  
+                equation.textContent = displayValue + " " + operator;
+                answer.textContent = displayValue;
             }
-            else if(lastValue === "firstNum" && btn.textContent === "-") {
-                firstNum += btn.textContent; 
-                displayValue = firstNum; 
-                answer.textContent = displayValue; 
-            } 
-            else if(isOperator(btn.textContent)){
-                operator = btn.textContent; 
-                equation.textContent = firstNum + " " + operator; 
+            else if (lastValue === "firstNum" && btn.textContent === "-") {
+                firstNum += btn.textContent;
+                displayValue = firstNum;
+                answer.textContent = displayValue;
+            }
+            else if (isOperator(btn.textContent)) {
+                operator = btn.textContent;
+                equation.textContent = firstNum + " " + operator;
                 lastValue = "operator";
             }
-            else if(lastValue === "firstNum") { 
-                firstNum += btn.textContent; 
-                displayValue = firstNum; 
+            else if (lastValue === "firstNum") {
+                firstNum += btn.textContent;
+                displayValue = firstNum;
                 disableDecimal(displayValue, decimalButton);
-                answer.textContent = displayValue; 
+                answer.textContent = displayValue;
             }
             else {
-                secondNum += btn.textContent; 
-                if(lastValue === "operator") {
-                    displayValue = ""; 
+                if(operator === "÷" && btn.textContent === "0") {
+                    alert("Cannot divide by 0!"); 
                 }
-                lastValue = "secondNum"; 
-                displayValue = secondNum; 
-                disableDecimal(displayValue, decimalButton); 
-                answer.textContent = displayValue; 
+                else {
+                    secondNum += btn.textContent;
+                    lastValue = "secondNum";
+                    displayValue = secondNum;
+                    disableDecimal(displayValue, decimalButton);
+                    answer.textContent = displayValue;
+                }
             }
-        });
+        }); 
     });
 }
 
